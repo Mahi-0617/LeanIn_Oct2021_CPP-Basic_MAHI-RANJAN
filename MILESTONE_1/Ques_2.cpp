87c4c7172eb297b298f6bb99ba3469d191f025d5
MAHI RANJAN <87914168+Mahi-ranjaN@users.noreply.github.com>
2021-10-10 08:40:22 +0530

    Add files via upload

---
+++ MILESTONE_1/Ques_2.cpp
@@ -0,0 +1,12 @@
+//Write a program to print a number entered by user
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    float Entered_number;
+    cout<<"Enter any number which you want to print\n";
+    cin>> Entered_number;
+    cout<< "Your entered number is : "<<Entered_number;
+    return 0;
+}
