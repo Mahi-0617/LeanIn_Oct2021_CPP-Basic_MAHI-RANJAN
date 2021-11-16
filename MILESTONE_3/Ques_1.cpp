a8b22d5dcf55353ddce27a38e023a98f498b9912
MAHI RANJAN <87914168+Mahi-ranjaN@users.noreply.github.com>
2021-11-16 11:22:00 +0530

    Add files via upload

---
+++ MILESTONE_3/Ques_1.cpp
@@ -0,0 +1,15 @@
+//1. Create a character array of size n and display its content.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[100];
+
+    cout << "Enter a string: ";
+    cin >> name;
+    cout << "You entered: " << name << endl;
+
+     return 0;
+}
